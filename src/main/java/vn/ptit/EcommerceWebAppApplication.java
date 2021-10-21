8b7174e3daa9317d1bd90e0c6e8d0724b54f1ec8
Cuong Pham <phamcuongth2000@gmail.com>
2021-10-21 16:14:30 +0700

    Initial commit

---
+++ src/main/java/vn/ptit/EcommerceWebAppApplication.java
@@ -0,0 +1,13 @@
+package vn.ptit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/vn/ptit/configs/WebMVC.java
@@ -0,0 +1,58 @@
+package vn.ptit.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Day la file cau hinh Spring MVC cho project
+ * 
+ * @author admin
+ */
+
+@Configuration /* bao hieu cho spring boot biet day la file cau hinh */
+@EnableWebMvc /* file cau hinh nay dung cho Web MVC */
+@ComponentScan(basePackages = { "vn.ptit" }) /* chi ro cho spring mvc biet noi chua cac controller */
+public class WebMVC implements WebMvcConfigurer {
+
+	/**
+	 * chỉ cho spring-mvc biết nơi chứa các view đã tạo ở phần 1: front-end.
+	 * 
+	 * @return
+	 */
+	
+	@Value("${file.upload.path}")
+	private String attachmentPath;
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/views/");
+		bean.setSuffix(".jsp");
+		return bean;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+		registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+//		registry.addResourceHandler("/favicons/**").addResourceLocations("classpath:/static/favicons/");
+		registry.addResourceHandler("/files_bookitem/**").addResourceLocations(attachmentPath);
+		registry.addResourceHandler("/files_electronicsitem/**").addResourceLocations(attachmentPath);
+		registry.addResourceHandler("/files_shoesitem/**").addResourceLocations(attachmentPath);
+		registry.addResourceHandler("/files_clothesitem/**").addResourceLocations(attachmentPath);
+//		registry.addResourceHandler("/files_banners/**").addResourceLocations(attachmentPath);
+//		registry.addResourceHandler("/files_users/**").addResourceLocations(attachmentPath);
+		
+	}
+
+}
